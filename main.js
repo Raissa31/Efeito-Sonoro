677867c1a36777b7d8e434e2292f7b69f4b18e0b
Raissa31 <105080642+Raissa31@users.noreply.github.com>
2023-10-27 10:29:10 -0300

    62

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ function TocaSomPom(){
 //chamda ou invocação da função tocaSomPom ao clicar no botão
 document.querySelector('tecla_pom').onclick = TocaSomPom;
 
+let contador = 0;
+white(contador < 9){
+    litaDeTeclas[0]. onclick = tocaSomPom;
+    contador = contador + 1;
+    console.long(contador);
+}
