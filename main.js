52ff649a31ef8f7b8e66b2ac0e21d8c0cf4b9146
Raissa31 <105080642+Raissa31@users.noreply.github.com>
2023-10-27 10:09:52 -0300

    16

--- main.js
+++ main.js
@@ -5,10 +5,3 @@ function TocaSomPom(){
 //chamda ou invocação da função tocaSomPom ao clicar no botão
 document.querySelector('tecla_pom').onclick = TocaSomPom;
 
-//Declaração/criação da função tocaSomClap
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-
-//chamada ou invocação da função tocaSomClap ao clipe no botão
-document.querySelector('.tecla_clap').onclick = tocaSomClap
-}
