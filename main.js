30c232adfbc56e4219f085667d31e6d2b6bfdc7a
Raissa31 <105080642+Raissa31@users.noreply.github.com>
2023-11-10 11:10:54 -0300

    12

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 function TocaSomPom(){
     document.querySelector('#som_tecla_pom'). play();
 }
-//chamda ou invocação da função tocaSomPom ao clicar no botão
+//criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 document.querySelector('tecla_pom').onclick = TocaSomPom;
-
+//criação da repetição " while" trocando pelo "for" com variavel, condição e incremento
 
 for(let contador = 0;contador < istaDeTeclas.length; contador = contador ++) {
-    const teclas = listaDeTeclas[contador]
-    const efeito = teclas.classlist[1];
-    const idAudio = `"#som_"  ${efeito;}`"
-    litaDeTeclas[contador].onclick = function(){
-        tocaSomPom(idAudio);
+    const teclas = listaDeTeclas[contador]//guarda o contador da lista
+    const efeito = teclas.classlist[1];//chama o 2° item classe individual do som 
+    const idAudio = `"#som_"  ${efeito}`;//" utiliza a junção templete tring ${ }
+    litaDeTeclas[contador].onclick = function(){ //função anônima auxiliar para chamar tocaSom
+        tocaSomPom(idAudio);//chamada de função tocaSom com parâmetro IdAudio
     }
     
     console.long(contador);
