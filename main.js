a11fa8ccf38fc1573dde7abf371b27d6f2717313
Raissa31 <105080642+Raissa31@users.noreply.github.com>
2023-11-24 11:15:07 -0300

    26

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Declaração/criação da função TocaSom
-function TocaSomPom(){
-    document.querySelector('#som_tecla_pom'). play();
+function TocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 //criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
-document.querySelector('tecla_pom').onclick = TocaSomPom;
+
 //criação da repetição " while" trocando pelo "for" com variavel, condição e incremento
 
 for(let contador = 0;contador < istaDeTeclas.length; contador = contador ++) {
@@ -11,18 +11,21 @@ for(let contador = 0;contador < istaDeTeclas.length; contador = contador ++) {
     const efeito = teclas.classlist[1];//chama o 2° item classe individual do som 
     const idAudio = `"#som_"  ${efeito}`;//" utiliza a junção templete tring ${ }
     litaDeTeclas[contador].onclick = function(){ //função anônima auxiliar para chamar tocaSom
-        tocaSomPom(idAudio);//chamada de função tocaSom com parâmetro IdAudio
+        tocaSom(idAudio);//chamada de função tocaSom com parâmetro IdAudio
     }
     console.long(contador);
     teclas.onkeydwon = function(evento){
-        if('Espaço'){
+
+        console.log(evento.code === 'Space')
+
+        if(evento.code == 'Space'){
 teclas.classlist.add('ativa');
         }
         teclas.classlist.add('ativa')
-    }if(evento.code != 'Tab'){
+    if(evento.code != 'Tab'){
         teclas.classlist.add('ativa')
     }
-
+    
     console.long(contador);
     teclas.onkeydwon = function(){
         teclas.classlist.add('ativa')
